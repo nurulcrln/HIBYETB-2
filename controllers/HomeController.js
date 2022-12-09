d257d3d0d6921af44461714958aa34ce6dafc5aa
nurulcrln <nurulcarolina14@gmail.com>
2022-12-09 18:26:49 +0700

    Perubahan homepage

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,5 +1,11 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/prefer-default-export */
+// import Artikel from '../models/Artikel';
+
 export const index = async (req, res) => {
   res.render('pages/homepage/home');
 };
+// export const index = async (req, res) => {
+//   const rows = await Artikel.findAll();
+//   res.render('pages/homepage/home', { data: rows });
+// };
--- index.js
+++ index.js
@@ -12,7 +12,9 @@ import UserRoute from './routes/UserRoute.js';
 import DokterRoute from './routes/DokterRoute.js';
 import ArtikelRoute from './routes/ArtikelRoute.js';
 import ReminderRoute from './routes/ReminderRoute.js';
-import HomepageRoute from './routes/HomepageRouter.js';
+import HomepageRoute from './routes/HomepageRoute.js';
+import LoginRoute from './routes/LoginRouter.js';
+import RegisterRoute from './routes/RegisterRouter.js';
 
 const app = express();
 
